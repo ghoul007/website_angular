fb5652e4e7d2c971d3f1055b197de245f1e69c39
ahmed <ahmedgh@rosafian.com>
2019-01-15 22:46:29 +0100

    template integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ClientComponent } from './client/client.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavigationComponent } from './navigation/navigation.component';
+ 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialComponent,
+    ClientComponent,
+    PricingComponent,
+    FooterComponent,
+    HeaderComponent,
+    SocialComponent,
+    NavigationComponent,
+ 
   ],
   imports: [
     BrowserModule
